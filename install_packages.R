450aa73cdd8f1eb083d54ae26c4e156ac794bbcb
Henryjean <owenlhjphillips@gmail.com>
2021-11-08 07:56:20 -0500

    fix tabulizer load, add old data

--- install_packages.R
+++ install_packages.R
@@ -7,4 +7,9 @@ local({
 install.packages("dplyr")
 install.packages("lubridate")
 install.packages("tidyr")
-install.packages("tabulizer")
+
+if (!require("remotes")) {
+  install.packages("remotes")
+}
+# on 64-bit Windows
+remotes::install_github(c("ropensci/tabulizerjars", "ropensci/tabulizer"), INSTALL_opts = "--no-multiarch")
